9a2aa849c35b65d66cd75bfd68c6fdcddda9ec4c
jcardwell <jrcardwell16@gmail.com>
2016-11-06 19:01:24 -0500

    Initial commit of the XCode project

---
+++ MenuMate/MenuMate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MenuMate
+//
+//  Created by Jack Cardwell on 11/6/16.
+//  Copyright © 2016 HarmonCardwell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
