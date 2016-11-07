e4e3629bfa4bf3040973fd776fa20034a97166a2
jcardwell <jrcardwell16@gmail.com>
2016-11-06 22:36:22 -0500

    Still struggling with the text box. Will come back to it tomorrow.

--- MenuMate/MenuMate/ViewController.swift
+++ MenuMate/MenuMate/ViewController.swift
@@ -8,29 +8,54 @@
 
 import UIKit
 
-class ViewController: UIViewController, UISearchBarDelegate {
+class ViewController: UIViewController, UITextFieldDelegate {
     //MARK: Properties
-    @IBOutlet weak var queryBar: UISearchBar!
+    @IBOutlet var searchField: UITextField!
+
+    var query=""      //the string of the query
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        //setup delegates
-        queryBar.delegate = self;
+        
+        searchField.delegate=self;
+        //setup the default values of the text field
+        searchField.clearsOnBeginEditing=true;
+        searchField.clearsOnInsertion=true;
     }
     
-    //a method that gathers the text from the search field when the user finishes editing
-    func searchBar(_ searchBar: UISearchBar, searchBarDidEndEditing searchText: String) {
-        
-        print(searchText)
-        
-    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    //MARK: UITextFieldDelegate
+    //a function that resigns the first responder status when the keyboard is escaped
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        // Hide the keyboard.
+        switch textField {
+        case searchField:
+            searchField.resignFirstResponder()
+        default:
+            textField.resignFirstResponder()
+        }
+        return true
+    }
+    
+    //a function that updates the query text when the return key is pressed
+    func textFieldShouldEndEditing(textField: UITextField) {
+        
+        switch textField{
+            
+        case searchField:
+            query = textField.text!
+            print(query)
+            
+        default:
+            break
+        }
+    }
 
 }
 
